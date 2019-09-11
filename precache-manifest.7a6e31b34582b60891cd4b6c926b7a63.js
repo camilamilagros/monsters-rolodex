f7cfcb0c2825c59cb7e021678ae4b07b2e68b22e
Camila <camila.chavez@pucp.pe>
2019-09-10 22:42:39 -0500

    Updates

---
+++ precache-manifest.7a6e31b34582b60891cd4b6c926b7a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8582d4a3248e69e982ffe0f777478223",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4eb2f63bfb9909fdddbf",
+    "url": "/monsters-rolodex/static/css/main.f4da54be.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "4eb2f63bfb9909fdddbf",
+    "url": "/monsters-rolodex/static/js/main.2a4cd131.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
